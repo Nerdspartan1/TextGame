9b6d6d727270d55ad0d69b06862e37bfc2615ded
Nerdspartan1 <o.cornet2@gmail.com>
2019-02-05 23:18:53 +0100

    Added PlayerBar

---
+++ Assets/Scripts/FightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour
+{
+	//Singleton instance
+	private static FightManager instance = null;
+
+	public static float fightSceneWidth = 600;
+	public static float fightSceneHeight = 600;
+
+	public Transform fightPanel;
+
+	public GameObject playerObject;
+	private PlayerBar playerBar;
+
+	void Start()
+	{
+		playerBar = playerObject.GetComponent<PlayerBar>();
+		//Singleton
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	void BeginFight(Unit foe)
+	{
+		fightPanel.gameObject.SetActive(true);
+	}
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBar : MonoBehaviour
+{
+	public float speed;
+	private RectTransform rectTransform;
+
+	private void Start()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		float X = transform.localPosition.x;
+		X += speed * Input.GetAxisRaw("Horizontal")*Time.deltaTime;
+		X = Mathf.Clamp(X,-FightManager.fightSceneWidth/2, FightManager.fightSceneWidth / 2);
+		transform.localPosition = new Vector3(X, -FightManager.fightSceneHeight/2, 0);
+    }
+}
