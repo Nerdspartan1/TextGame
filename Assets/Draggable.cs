d52538aed34bc1bf00da8b42ab4cee26b9d7fd0b
Olivier Cornet <olivier.cornet@middlevr.com>
2019-06-24 15:18:00 +0200

    Add feature to click & drag inventory window

---
+++ Assets/Draggable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+	private bool dragging = false;
+	public RectTransform canvasRect;
+
+	private Vector3 dragPointLocalPosition;
+
+	new private Camera camera;
+	
+	private void Awake()
+	{
+		camera = Camera.main;
+	}
+
+	public void ToggleDrag(bool drag)
+	{
+		dragging = drag;
+		if (drag)
+		{
+			Vector3 position;
+
+			RectTransformUtility.ScreenPointToWorldPointInRectangle(canvasRect, Input.mousePosition, camera, out position);
+
+			dragPointLocalPosition = position - transform.position;
+		}
+	}
+	private void Update()
+	{
+		if (dragging)
+		{
+			Vector3 position;
+
+			RectTransformUtility.ScreenPointToWorldPointInRectangle(canvasRect, Input.mousePosition, camera, out position);
+
+			transform.position = position - dragPointLocalPosition;
+		}
+	}
+}
