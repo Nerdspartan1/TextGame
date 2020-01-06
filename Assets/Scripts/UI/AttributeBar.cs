744760aca399efe4d8c6e49dda5eca215fc76ed4
Nerdspartan1 <o.cornet2@gmail.com>
2020-01-06 22:52:37 +0100

    Add more readable attribute bar

--- Assets/Scripts/UI/AttributeBar.cs
+++ Assets/Scripts/UI/AttributeBar.cs
@@ -8,5 +8,44 @@ public class AttributeBar : StatBar
 	public Attribute Attribute;
 
 	[Header("References")]
+	public RectTransform UpperBarTransform;
 	public Button LevelUpButton;
+
+	public float OneBarValue;
+	public Gradient Gradient;
+
+	private Image upperBarImage;
+	private Image barImage;
+
+	public void Start()
+	{
+		upperBarImage = UpperBarTransform.GetComponent<Image>();
+		barImage = BarTransform.GetComponent<Image>();
+	}
+
+	public override void UpdateBar()
+	{
+		float bottomRatio;
+		float upperRatio;
+		if (Value < OneBarValue) {
+			bottomRatio = Value / OneBarValue;
+			upperRatio = 0;
+			barImage.color = Gradient.Evaluate(0);
+		}
+		else
+		{
+			barImage.color = Gradient.Evaluate(((int)(Value / OneBarValue)-1)*OneBarValue / MaxValue);
+			upperBarImage.color = Gradient.Evaluate((int)(Value / OneBarValue)*OneBarValue / MaxValue);
+			bottomRatio = 1;
+			upperRatio = (Value % OneBarValue) / OneBarValue;
+		}
+
+		BarTransform.localScale = new Vector3(bottomRatio > 0 ? bottomRatio : 0, 1, 1);
+		UpperBarTransform.localScale = new Vector3(upperRatio > 0 ? upperRatio : 0, 1, 1);
+
+		if (ValueText)
+		{
+			ValueText.text = $"{Value}";
+		}
+	}
 }
--- Assets/Scripts/UI/StatBar.cs
+++ Assets/Scripts/UI/StatBar.cs
@@ -11,17 +11,14 @@ public class StatBar : MonoBehaviour
 	public float Value;
 	public float MaxValue = 1;
 
-	public bool ShowMaxValue = true;
-
-	public void UpdateBar()
+	public virtual void UpdateBar()
 	{
 
 		float ratio = MaxValue == 0 ? 0 : Value / MaxValue;
 		BarTransform.localScale = new Vector3(ratio > 0 ? ratio : 0, 1, 1);
 		if (ValueText)
 		{
-			if (ShowMaxValue) ValueText.text = $"{Value} / {MaxValue}";
-			else ValueText.text = $"{Value}";
+			ValueText.text = $"{Value} / {MaxValue}";
 		}
 	}
 }
